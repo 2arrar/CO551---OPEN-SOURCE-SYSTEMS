311f5a66f2b107a215a39098987918a671978ff7
2arrar <91596670+2arrar@users.noreply.github.com>
2023-02-14 13:18:55 +0000

    Fixed wk2ex9.php

--- WEEK 2/Evidence/wk2ex9.php
+++ WEEK 2/Evidence/wk2ex9.php
@@ -1,11 +1,12 @@
 <?php
-$topmodules[0] = "Internet Systems Development";
-$topmodules[5] = "Programming 1";
-$topmodules[10] = "Programming 2";
-$topmodules[30] = "OOAD";
-$topmodules[40] = "Software Engineering";
+  $topmodules[0] = "Internet Systems Development";
+  $topmodules[5] = "Programming 1";
+  $topmodules[10] = "Programming 2";
+  $topmodules[30] = "OOAD";
+  $topmodules[40] = "Software Engineering";
 
-foreach($topmodules as $x => $val)
-{
-    echo "Module Number:$x is $val<br>";
-}
+  foreach($topmodules as $index => $value)
+  {
+    echo "Index is $index and value is $value <br/>";
+  }
+?>
